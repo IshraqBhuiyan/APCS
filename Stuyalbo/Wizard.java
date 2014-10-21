4d4a2c8993af4fcbc840aeb9dc0b5e11ec222e0e
ishraqbhuiyan <ishraqbhuiyan@gmail.com>
2014-10-21 15:25:37 +0000

    update Wizard class

--- Stuyalbo/Wizard.java
+++ Stuyalbo/Wizard.java
@@ -1,18 +1,36 @@
+import java.util.Random;
+
 public class Wizard extends Adventurer{
     private int Mana;
+    private Random rand = new Random();
 
     public Wizard(){
-	super("Tim");
+	this("Tim");
+    }
+    
+    public Wizard(String name){
+	this(name, 20);
+    }
+    
+    public Wizard(String name, int mana){
+	super(name, 20, 0, 0, 10);
+	this.setMana(mana);
     }
 
     //wizard methods
 
-    public void Attack(Adventurer other){
-
-    }
 
     public void SpecialAttack(Adventurer other){
-	
+	int damage = rand.nextInt(5) + this.INT / 2;
+	if(super.hit(other, 5)){
+	    other.setHP(other.getHP - damage);
+	    this.setMana(this.getMana - 2);
+	    System.out.println(this.getName() + " hit " + other.getName() + " and did " + damage + " damage");
+	}else{
+	    System.out.println(this.getName() + " missed " + other.getName());
+	}
+	System.out.println(this.getStats());
+	System.out.println(other.getStats());
     }
 
     //get methods
